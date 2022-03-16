0af8ddfc9d09d6d07eaeff916e8259efbcb34a53
Danilo Romano <risadex@gmail.com>
2022-03-16 16:08:36 -0300

    finish service tests
    Signed-off-by: Danilo Romano <risadex@gmail.com>

--- tests/unit/server/service.test.ts
+++ tests/unit/server/service.test.ts
@@ -33,24 +33,50 @@ describe("#Service - test service implementation", () => {
 
   })
 
-  test("#createFileStream", async () => {
+  test("#getFileInfo", async () => {
     const filename = "test.png"
+    const mockFilePath = `${publicDirectory}/${filename}`
+
+    const service = new Service();
+
+    jest.spyOn(fsPromises, fsPromises.access.name as "access").mockResolvedValue()
+
+    const result = await service.getFileInfo(filename)
+
+    const expectedResult = {
+      type: ".png",
+      name: mockFilePath
+    }
+
+    expect(expectedResult).toStrictEqual(result)
+
+
+
+  })
+
+  test("#getFileStream", async () => {
+    const filename = "video.mp4"
+    const readableStream = TestUtil.generateReadableStream([filename])
     const [, extension] = filename.split(".")
 
-    const mockFilePath = `${publicDirectory}/${extension}`
-    
+    const mockFilePath = `${publicDirectory}/${filename}`
+    const service = new Service();
     jest.spyOn(fsPromises, fsPromises.access.name as "access").mockResolvedValue()
     jest.spyOn(path, path.extname.name as "extname").mockReturnValue(extension)
     jest.spyOn(path, path.join.name as "join").mockReturnValue(mockFilePath)
-    
-    const service = new Service();
-
-    const { name, type } = await service.getFileInfo(filename)
 
 
-    expect(path.extname).toHaveBeenCalledWith(mockFilePath)
-    expect(name).toEqual(mockFilePath)
-    expect(type).toEqual(extension)
+    jest.spyOn(fs, fs.createReadStream.name as "createReadStream").mockReturnValue(readableStream)
+
+    const expectedResult = {
+      stream:readableStream,
+      type: extension
+    }
+
+    const streamResult = await service.getFileStream(filename)
+
+    expect(streamResult).toStrictEqual(expectedResult)
+    expect(fs.createReadStream).toHaveBeenCalledWith(mockFilePath)
 
 
   })
